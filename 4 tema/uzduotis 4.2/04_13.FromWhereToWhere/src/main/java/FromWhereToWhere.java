3406253832594804f42b8982161ef0e4bbfd4593
[Agne Balciunaite] <[agne.balciunaite@stud.techin.lt]>
2025-02-26 14:05:12 +0100

    4 temos 4.2 namu darbai, uzbaigti

--- 4 tema/uzduotis 4.2/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ 4 tema/uzduotis 4.2/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -1,4 +1,6 @@
 
+// import com.sun.org.apache.xml.internal.utils.SystemIDResolver;
+
 import java.util.Scanner;
 
 public class FromWhereToWhere {
@@ -7,5 +9,14 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Where to?");
+        int numberEnd = scanner.nextInt();
+
+        System.out.println("Where from?");
+        int numberStart = scanner.nextInt();
+
+        for (int i = numberStart; i <= numberEnd; i++){
+            System.out.println(i);
+        }
     }
 }
--- 4 tema/uzduotis 4.2/04_14.SumOfASequence/src/main/java/SumOfASequence.java
+++ 4 tema/uzduotis 4.2/04_14.SumOfASequence/src/main/java/SumOfASequence.java
@@ -6,5 +6,14 @@ public class SumOfASequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Last number?");
+        int lastNumber = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i <= lastNumber; i++){
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
